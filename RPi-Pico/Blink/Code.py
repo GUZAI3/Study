cb20ca08b8aa782e300a52375476539fdafb49c1
GUZAI3 <55838958+GUZAI3@users.noreply.github.com>
2022-04-13 23:16:18 +0900

    Add files via upload

---
+++ RPi-Pico/Blink/Code.py
@@ -0,0 +1,20 @@
+import machine
+import utime
+led1 = machine.Pin(3, machine.Pin.OUT)
+led2 = machine.Pin(4, machine.Pin.OUT)
+led3 = machine.Pin(5, machine.Pin.OUT)
+while True:
+    led1.value(1)   # LED1 ON
+    led2.value(0)   # LED2 OFF
+    led3.value(0)   # LED3 OFF
+    utime.sleep(1)  # 1s stop
+
+    led1.value(0)   # LED1 OFF
+    led2.value(1)   # LED2 ON
+    led3.value(0)   # LED3 OFF
+    utime.sleep(1)  # 1s stop
+    
+    led1.value(0)   # LED1 OFF
+    led2.value(0)   # LED2 OFF
+    led3.value(1)   # LED3 ON
+    utime.sleep(1)  # 1s stop
